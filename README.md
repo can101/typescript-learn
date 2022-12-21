5a26610c90dbd7ef34427bbd6f5887f16c03467a
can101 <can896373@gmail.com>
2022-12-22 02:55:04 +0300

    11 lesson add

--- README.md
+++ README.md
@@ -242,3 +242,34 @@ logDetails = (ninja: person) => {
 }
 
 ```
+## lesson 11 `The DOM & Type casting`
+> HTMLSelectElement,HTMLInputElement,HTMLFormElement,valueAsNumber,Event
+```ts
+
+const anchor = document.querySelector('a');
+
+// console.log('====================================');
+// console.log(anchor?.href);
+// console.log('====================================');
+
+// if (anchor) {
+//     console.log(anchor?.href);
+// }
+
+// console.log(anchor?.href);
+
+// const form=document.querySelector('form');
+const form = document.querySelector('.new-item-form') as HTMLFormElement;
+console.log(form.children);
+
+const type = document.querySelector('#type') as HTMLSelectElement;
+const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
+const details = document.querySelector('#details') as HTMLInputElement;
+const amount = document.querySelector('#amount') as HTMLInputElement;
+
+
+form.addEventListener('submit', (e: Event) => {
+    e.preventDefault();
+    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+})
+```
--- src/sandbox.js
+++ src/sandbox.js
@@ -1,22 +1,53 @@
-// let greet = () => {
-//     console.log("hello , world");
+// // let greet = () => {
+// //     console.log("hello , world");
+// // }
+// // greet='hello';
+// let greet: Function;
+// // greet="dsgfdhg"; //Type 'string' is not assignable to type 'Function'  
+// greet = () => {
+//     console.log("hello,again");
 // }
-// greet='hello';
-var greet;
-// greet="dsgfdhg"; //Type 'string' is not assignable to type 'Function'  
-greet = function () {
-    console.log("hello,again");
+// const add = (a: number, b: number, c: number | string = 10): void => {
+//     // const add = (a: number, b?: number | string) => {
+//     console.log(a + b);
+//     console.log(c);
+// }
+// // add(5, 10)
+// add(5, 10, "fdgfd")
+// const minus = (a: number, b: number): number => {
+//     return a + b;
+// }
+// let result = minus(10, 7);
+// // result="sumthing else"// Type 'string' is not assignable to type 'number'.
+// type stringORNumber = string | number
+// type objectWithName = { name: string, uid: stringORNumber }
+// const logDetails = (uid: stringORNumber, item: string) => {
+//     console.log(`${item} has a uid of ${uid}`);
+// }
+// const greet = (user: objectWithName) => {
+//     console.log(`${user.name} says hello`);
+// }
+// const greetAgain = (user: objectWithName) => {
+//     console.log(`${user.name} says hello`);
+// }
+// let grret:Function;
+// example 1
+var greet; // signature
+greet = function (name, greeeting) {
+    console.log(name + " says " + greeeting);
 };
-var add = function (a, b, c) {
-    if (c === void 0) { c = 10; }
-    // const add = (a: number, b?: number | string) => {
-    console.log(a + b);
-    console.log(c);
+// example 2
+var calc; // signature
+calc = function (numOne, numTwo, action) {
+    if (action === "add") {
+        return numOne + numTwo;
+    }
+    else {
+        return numOne - numTwo;
+    }
 };
-// add(5, 10)
-add(5, 10, "fdgfd");
-var minus = function (a, b) {
-    return a + b;
+// example 3
+var logDetails;
+logDetails = function (ninja) {
+    console.log("".concat(ninja, " is ").concat(ninja.age, " years old"));
 };
-var result = minus(10, 7);
-// result="sumthing else"// Type 'string' is not assignable to type 'number'.
